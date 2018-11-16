abf04dada3df1096f5a20b89084627dd60268b4a
Nolan-Little <little.nolan@gmail.com>
2018-11-16 13:51:45 -0600

    add animals component to display animals

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import EmployeeList from './employee/employee'
 import LocationList from './location/location'
+import AnimalList from './animal/animal'
 import "./Kennel.css"
 
 export default class Kennel extends Component {
@@ -17,11 +18,19 @@ export default class Kennel extends Component {
     { id: 2, name: "Nashville South", address: "298 Doge Blvd." }
   ]
 
+  animalsFromAPI = [
+    { id: 1, name: "Frank", species: "Dog" },
+    { id: 2, name: "Scratchy", species: "Cat" },
+    { id: 3, name: "Sneaky", species: "Ferret" },
+    { id: 4, name: "Tubby", species: "Rabbit" },
+    { id: 5, name: "Gordon", species: "Dog" }
+  ]
 
 
   state = {
     employees: this.employeesFromAPI,
-    locations: this.locationsFromAPI
+    locations: this.locationsFromAPI,
+    animals: this.animalsFromAPI
   }
 
   render() {
@@ -29,6 +38,7 @@ export default class Kennel extends Component {
       <article className="kennel">
         <LocationList locations={this.state.locations} />
         <EmployeeList employees={this.state.employees} />
+        <AnimalList animals={this.state.animals}/>
       </article>
     )
   }
