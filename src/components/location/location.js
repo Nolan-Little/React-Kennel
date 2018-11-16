b7e27a7cb8c1f537247c02d31e5ac22a7286ee51
Nolan-Little <little.nolan@gmail.com>
2018-11-16 13:25:54 -0600

    complete in class exercise

---
+++ src/components/location/location.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+export default class Location extends Component {
+  render() {
+    return (
+      <article>
+        <h1>Kennel Locations</h1>
+        <section>
+          <h2>Nashville North</h2>
+          <h5>500 Puppy Way</h5>
+        </section>
+        <section>
+          <h2>Nashville East</h2>
+          <h5>600 Doge Blvd.</h5>
+        </section>
+      </article>
+    )
+  }
+}
